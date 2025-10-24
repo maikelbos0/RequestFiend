1fd6390326387e7c762fd39098f6063b8f5c4649
Maikel Bos <maikel.bos0@gmail.com>
2025-10-24 13:17:55 +0200

    Add first template classes

---
+++ src/RequestFiend.Core/RequestTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace RequestFiend.Core;
+
+public class RequestTemplate
+{
+    public string? Name { get; set; }
+    public string? Url { get; set; }
+    public HttpMethod? HttpMethod { get; set; }
+}
---
+++ src/RequestFiend.Core/RequestTemplateCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace RequestFiend.Core;
+
+public class RequestTemplateCollection {
+    public string? Name { get; set; }
+    public string? BaseUrl { get; set; }
+    public List<RequestTemplate> RequestTemplates { get; set; } = [];
+}
