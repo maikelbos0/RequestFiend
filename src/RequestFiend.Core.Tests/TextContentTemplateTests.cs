b7afe73b31f871a36223092f366a635d6a9be5c2
Maikel Bos <maikel.bos0@gmail.com>
2025-10-26 20:00:38 +0100

    Add text content

---
+++ src/RequestFiend.Core.Tests/TextContentTemplateTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace RequestFiend.Core.Tests;
+
+public class TextContentTemplateTests {
+    [Fact]
+    public void Validate() {
+        var subject = new TextContentTemplate() {
+            Text = "Just a piece of text"
+        };
+        var collection = new RequestTemplateCollection() {
+            Name = "Collection"
+        };
+
+        Assert.True(subject.Validate(collection));
+    }
+
+    [Fact]
+    public void Format() {
+        const string text = "Just a piece of text";
+        var subject = new TextContentTemplate() {
+            Text = text
+        };
+        var collection = new RequestTemplateCollection() {
+            Name = "Collection"
+        };
+
+        subject.Format(collection);
+
+        Assert.Equal(text, subject.Text);
+    }
+
+    [Fact]
+    public void GetContent() {
+        var subject = new TextContentTemplate() {
+            Text = "The {{first}} and {{second}} get replaced"
+        };
+        var collection = new RequestTemplateCollection() {
+            Name = "Collection",
+            Variables = {
+                { "First", "Replacement" },
+                { "Second", "Another" }
+            }
+        };
+
+        Assert.Equal("The Replacement and Another get replaced"u8.ToArray(), subject.GetContent(collection));
+    }
+}
---
+++ src/RequestFiend.Core/TextContentTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace RequestFiend.Core;
+
+public class TextContentTemplate : IContentTemplate {
+    public const string DefaultMediaType = "text/plain";
+
+    public string MediaType { get; set; } = DefaultMediaType;
+    public string? CharSet { get; } = Encoding.UTF8.WebName;
+    public required string Text { get; set; }
+
+    public bool Validate(RequestTemplateCollection collection)
+        => true;
+
+    public void Format(RequestTemplateCollection collection) { }
+
+    public byte[] GetContent(RequestTemplateCollection collection)
+        => Encoding.UTF8.GetBytes(collection.ApplyVariables(Text));
+}
