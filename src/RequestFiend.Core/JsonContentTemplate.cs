78d2a2395b3665c5bdca4b0618b9558774dd2b1e
Maikel Bos <maikel.bos0@gmail.com>
2025-10-27 19:56:21 +0100

    Add json content with Validate and GetContent

---
+++ src/RequestFiend.Core/JsonContentTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace RequestFiend.Core;
+
+public class JsonContentTemplate : IContentTemplate {
+    public const string DefaultMediaType = "application/json";
+
+    public string MediaType { get; set; } = DefaultMediaType;
+    public string? CharSet { get; } = Encoding.UTF8.WebName;
+    public required string Content { get; set; }
+
+    public bool Validate(RequestTemplateCollection collection) {
+        try {
+            JsonDocument.Parse(collection.ApplyVariables(Content));
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+
+    public bool Format(RequestTemplateCollection collection)
+        => throw new NotImplementedException();
+
+    public byte[] GetContent(RequestTemplateCollection collection)
+        => Encoding.UTF8.GetBytes(collection.ApplyVariables(Content));
+}
