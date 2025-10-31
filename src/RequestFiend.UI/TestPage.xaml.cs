0c43a252bc9fd85c90337354552bbf386b05b486
Maikel Bos <maikel.bos0@gmail.com>
2025-10-31 13:09:20 +0100

    Trying to figure out shell flyouts and navigation

---
+++ src/RequestFiend.UI/TestPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace RequestFiend.UI;
+
+public partial class TestPage : ContentPage {
+    public TestPage() {
+        InitializeComponent();
+    }
+
+    private void OnAddClicked(object sender, System.EventArgs e) {
+        AppShell.Current.Items.Add(new ShellContent() {
+            Title = "Collection 3",
+            Content = new TestPage()
+        });
+    }
+}
