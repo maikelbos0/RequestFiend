8d861d79bdd9d238c3fb24722cd1012f19e0d60b
Maikel Bos <maikel.bos0@gmail.com>
2025-11-19 17:39:24 +0100

    Add headlines for table sections and hide heading rows

---
+++ src/RequestFiend.UI/GreaterThanZeroConverter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace RequestFiend.UI;
+
+public class GreaterThanZeroConverter : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is int count && count > 0;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
