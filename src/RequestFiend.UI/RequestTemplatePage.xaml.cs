fc4194309859f28b841469beeed0638b993f7fc6
Maikel Bos <maikel.bos0@gmail.com>
2025-11-20 19:29:21 +0100

    Add headers to request UI

--- src/RequestFiend.UI/RequestTemplatePage.xaml.cs
+++ src/RequestFiend.UI/RequestTemplatePage.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Maui.Controls;
 using RequestFiend.Core;
 using RequestFiend.Models;
 using RequestFiend.UI.Messages;
@@ -26,6 +27,14 @@ public partial class RequestTemplatePage : RequestTemplateCollectionPageBase<Req
 
         await SuccessMessage.Show("Changes have been saved");
     }
+
+    public void OnRemovetHeaderClicked(object sender, EventArgs e) {
+        var button = (Button)sender;
+
+        Model.Headers.Remove((HeaderTemplateModel)button.BindingContext);
+    }
+
+    public void OnAddHeaderClicked(object sender, EventArgs e) => Model.Headers.Add(new());
 
     private async void OnDeleteRequestClicked(object sender, EventArgs e) {
         var popupResult = await this.ShowPopupAsync<bool>(new Views.ConfirmPopup("Are you sure you want to delete this request?"));
