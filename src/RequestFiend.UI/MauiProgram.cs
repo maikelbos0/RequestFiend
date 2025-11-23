0912cb46547fd9ad5efb8cf0487c661f076a9d39
Maikel Bos <maikel.bos0@gmail.com>
2025-11-23 16:47:22 +0100

    Add mono font for code

--- src/RequestFiend.UI/MauiProgram.cs
+++ src/RequestFiend.UI/MauiProgram.cs
@@ -14,6 +14,7 @@ public static class MauiProgram {
             .ConfigureFonts(fonts => {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("FiraCode-Regular.ttf", "FiraCodeRegular");
             });
 
 #if DEBUG
