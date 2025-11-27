142e05d1ac02f7249f8948270883853fbabf324d
Maikel Bos <maikel.bos0@gmail.com>
2025-11-27 20:00:43 +0100

    Use new validatable views for new request fields

--- src/RequestFiend.Models/NewRequestTemplateModel.cs
+++ src/RequestFiend.Models/NewRequestTemplateModel.cs
@@ -5,24 +5,24 @@ using System.Diagnostics.CodeAnalysis;
 namespace RequestFiend.Models;
 
 public class NewRequestTemplateModel : BoundModelBase {
-    public RequiredString Name { get; set; } = new();
-    public RequiredString Method { get; set; } = new();
-    public RequiredString Url { get; set; } 
+    public ValidatableString Name { get; set; } = new(true);
+    public ValidatableString Method { get; set; } = new(true);
+    public ValidatableString Url { get; set; } 
 
     public NewRequestTemplateModel(RequestTemplateCollection collection) {
-        Url = new(() => collection.DefaultUrl);
+        Url = new(true, () => collection.DefaultUrl);
     }
 
     public bool TryCreateRequestTemplate([NotNullWhen(true)] out RequestTemplate? request) {
-        if (!Name.IsValid || !Method.IsValid || !Url.IsValid) {
+        if (Name.HasError || Method.HasError || Url.HasError) {
             request = null;
             return false;
         }
 
         request = new() {
-            Name = Name,
-            Method = Method,
-            Url = Url
+            Name = Name.Value!,
+            Method = Method.Value!,
+            Url = Url.Value!
         };
         return true;
     }
