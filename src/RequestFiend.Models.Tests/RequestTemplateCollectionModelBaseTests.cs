1e8d65b07e0d0bc3ec1fd7d462e4f52ae6cf67e5
Maikel Bos <maikel.bos0@gmail.com>
2025-12-01 20:45:03 +0100

    I guess we want this as a base class IDK

---
+++ src/RequestFiend.Models.Tests/RequestTemplateCollectionModelBaseTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace RequestFiend.Models.Tests;
+
+public class RequestTemplateCollectionModelBaseTests {
+    [Fact]
+    public void Constructor() {
+        var subject = new RequestTemplateCollectionModelBase(@"C:\Documents\External data requests.json", new());
+
+        Assert.Equal("External data requests", subject.Title);
+    }
+}
---
+++ src/RequestFiend.Models/RequestTemplateCollectionModelBase.cs
@@ -0,0 +1,17 @@
+﻿using RequestFiend.Core;
+using System.IO;
+
+namespace RequestFiend.Models;
+
+public class RequestTemplateCollectionModelBase : BoundModelBase {
+    protected readonly string filePath;
+    protected readonly RequestTemplateCollection collection;
+
+    public string Title { get; }
+
+    public RequestTemplateCollectionModelBase(string filePath, RequestTemplateCollection collection) {
+        this.filePath = filePath;
+        this.collection = collection;
+        Title = Path.GetFileNameWithoutExtension(filePath);
+    }
+}
