0cdd18e46ca6451cb4ee7ff6d8eb5faeb46161f6
Maikel Bos <maikel.bos0@gmail.com>
2025-12-02 20:13:35 +0100

    Fix tests

--- src/RequestFiend.Models.Tests/BoundModelBaseTests.cs
+++ src/RequestFiend.Models.Tests/BoundModelBaseTests.cs
@@ -6,20 +6,24 @@ namespace RequestFiend.Models.Tests;
 public class BoundModelBaseTests {
     [Theory]
     [MemberData(nameof(PageWidth_Sets_StackOProperties_Data))]
-    public void PageWidth_Sets_StackOProperties(double pageWidth, LayoutOptions expectedStackHorizontalOptions, StackOrientation expectedStackOrientation, bool expectedStackIsHorizontal, bool expectedStackIsVertical) {
+    public void PageWidth_Sets_StackOProperties(double pageWidth, bool expectedHorizontalFill, StackOrientation expectedStackOrientation, bool expectedStackIsHorizontal, bool expectedStackIsVertical) {
         var subject = new BoundModelBase() {
             PageWidth = pageWidth
         };
 
+        var expectedStackHorizontalOptions = expectedHorizontalFill ? LayoutOptions.Fill : LayoutOptions.End;
+
         Assert.Equal(expectedStackHorizontalOptions, subject.StackHorizontalOptions);
         Assert.Equal(expectedStackOrientation, subject.StackOrientation);
+        Assert.Equal(expectedStackIsHorizontal, subject.StackIsHorizontal);
+        Assert.Equal(expectedStackIsVertical, subject.StackIsVertical);
     }
 
-    public static TheoryData<double, LayoutOptions, StackOrientation, bool, bool> PageWidth_Sets_StackOProperties_Data()
+    public static TheoryData<double, bool, StackOrientation, bool, bool> PageWidth_Sets_StackOProperties_Data()
         => new() {
-            { 400, LayoutOptions.Fill, StackOrientation.Vertical, false, true },
-            { 674, LayoutOptions.Fill, StackOrientation.Vertical, false, true },
-            { 675, LayoutOptions.End, StackOrientation.Horizontal, true, false },
-            { 900, LayoutOptions.End, StackOrientation.Horizontal, true, false },
+            { 400, true, StackOrientation.Vertical, false, true },
+            { 674, true, StackOrientation.Vertical, false, true },
+            { 675, false, StackOrientation.Horizontal, true, false },
+            { 900, false, StackOrientation.Horizontal, true, false },
         };
 }
