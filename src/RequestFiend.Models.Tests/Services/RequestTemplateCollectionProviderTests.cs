00267aee99970cce0a60657daeeffaed1eb6c721
Maikel Bos <maikel.bos0@gmail.com>
2025-12-05 08:43:32 +0100

    Add way to resolve collection and filepath from service provider

---
+++ src/RequestFiend.Models.Tests/Services/RequestTemplateCollectionProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using RequestFiend.Core;
+using RequestFiend.Models.Services;
+using System;
+using Xunit;
+
+namespace RequestFiend.Models.Tests.Services;
+
+public class RequestTemplateCollectionProviderTests {
+    [Fact]
+    public void CreateScope_Sets_And_Clear_Data() {
+        const string filePath = @"C:\Documents\External data requests.json";
+        var collection = new RequestTemplateCollection();
+
+        var subject = new RequestTemplateCollectionProvider();
+
+        using (subject.CreateScope(filePath, collection)) {
+            Assert.True(subject.Data.HasValue);
+            Assert.Equal(filePath, subject.Data.Value.FilePath);
+            Assert.Equal(collection, subject.Data.Value.Collection);
+        }
+
+        Assert.False(subject.Data.HasValue);
+    }
+
+    [Fact]
+    public void CreateScope_Throws_For_Double_Scope() {
+        const string filePath = @"C:\Documents\External data requests.json";
+        var collection = new RequestTemplateCollection();
+
+        var subject = new RequestTemplateCollectionProvider();
+
+        using (subject.CreateScope(filePath, collection)) {
+            Assert.Throws<InvalidOperationException>(() => subject.CreateScope(filePath, collection));
+        }
+    }
+}
---
+++ src/RequestFiend.Models/Services/RequestTemplateCollectionProvider.cs
@@ -0,0 +1,29 @@
+﻿using RequestFiend.Core;
+using System;
+
+namespace RequestFiend.Models.Services;
+
+public class RequestTemplateCollectionProvider {
+    private class Scope : IDisposable {
+        private readonly RequestTemplateCollectionProvider provider;
+
+        public Scope(RequestTemplateCollectionProvider provider, string filePath, RequestTemplateCollection collection) {
+            this.provider = provider;
+            provider.Data = (filePath, collection);
+        }
+
+        public void Dispose() {
+            provider.Data = null;
+        }
+    }
+
+    public (string FilePath, RequestTemplateCollection Collection)? Data { get; private set; }
+
+    public IDisposable CreateScope(string filePath, RequestTemplateCollection collection) {
+        if (Data != null) {
+            throw new InvalidOperationException("Only one scope at a time is allowed; ensure previous scopes are disposed of");
+        }
+
+        return new Scope(this, filePath, collection);
+    }
+}
