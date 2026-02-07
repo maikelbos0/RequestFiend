cdf05993b916a3e2a9fae555724979a0ca2b7bbf
Maikel Bos <maikel.bos0@gmail.com>
2026-02-07 09:31:33 +0100

    Create generic validatable since we'll need it for more than just strings

---
+++ src/RequestFiend.Models.Tests/PropertyTypes/ValidatablePropertyTests.cs
@@ -0,0 +1,49 @@
+﻿using RequestFiend.Models.PropertyTypes;
+using Xunit;
+
+namespace RequestFiend.Models.Tests.PropertyTypes;
+
+public class ValidatablePropertyTests {
+    [Theory]
+    [InlineData(null, true)]
+    [InlineData("", true)]
+    [InlineData("Value", false)]
+    public void Constructor(string? initialValue, bool expectedHasError) {
+        var subject = new ValidatableProperty<string?>(() => initialValue, value => !string.IsNullOrEmpty(value));
+
+        Assert.Equal(initialValue, subject.Value);
+        Assert.False(subject.IsModified);
+        Assert.Equal(expectedHasError, subject.HasError);
+    }
+
+    [Theory]
+    [InlineData(null, null, false, true)]
+    [InlineData(null, "", false, true)]
+    [InlineData(null, "Changed", true, false)]
+    [InlineData("Initial", "Initial", false, false)]
+    [InlineData("Initial", "Changed", true, false)]
+    [InlineData("Initial", null, false, true)]
+    [InlineData("Initial", "", false, true)]
+    public void Value(string? initialValue, string? newValue, bool expectedIsModified, bool expectedHasError) {
+        var subject = new ValidatableProperty<string?>(() => initialValue, value => !string.IsNullOrEmpty(value)) {
+            Value = newValue
+        };
+
+        Assert.Equal(newValue, subject.Value);
+        Assert.Equal(expectedIsModified, subject.IsModified);
+        Assert.Equal(expectedHasError, subject.HasError);
+    }
+
+    [Fact]
+    public void Reset() {
+        const string initialValue = "Initial";
+        const string resetValue = "Reinitialized";
+
+        var subject = new ValidatableProperty<string?>(() => initialValue);
+
+        subject.Reset(() => resetValue);
+
+        Assert.Equal(resetValue, subject.Value);
+        Assert.False(subject.IsModified);
+    }
+}
---
+++ src/RequestFiend.Models/PropertyTypes/ValidatableProperty.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+
+namespace RequestFiend.Models.PropertyTypes;
+
+public class ValidatableProperty<TProperty> : ObservableObject {
+    public Func<TProperty> DefaultValueProvider { get; private set; }
+    public Func<TProperty, bool> Validator { get; }
+
+    public TProperty Value {
+        get => field;
+        set {
+            SetProperty(ref field, value);
+            HasError = !Validator(value);
+            IsModified = !HasError && !EqualityComparer<TProperty>.Default.Equals(value, DefaultValueProvider());
+        }
+    }
+    public bool IsModified {
+        get => field;
+        private set => SetProperty(ref field, value);
+    }
+    public bool HasError {
+        get => field;
+        private set => SetProperty(ref field, value);
+    }
+
+    public ValidatableProperty(Func<TProperty> defaultValueProvider) : this(defaultValueProvider, _ => true) { }
+
+    public ValidatableProperty(Func<TProperty> defaultValueProvider, Func<TProperty, bool>? validator) {
+        DefaultValueProvider = defaultValueProvider;
+        Validator = validator ?? (_ => false);
+        Value = DefaultValueProvider();
+    }
+
+    public void Reset(Func<TProperty> defaultValueProvider) {
+        DefaultValueProvider = defaultValueProvider;
+        Reset();
+    }
+
+    public void Reset() {
+        Value = DefaultValueProvider();
+    }
+}
