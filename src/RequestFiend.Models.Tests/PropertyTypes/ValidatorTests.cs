43b14a4a10c27a2012abc2812fb48ec8596f6608
Maikel Bos <maikel.bos0@gmail.com>
2026-02-07 10:01:54 +0100

    Add validators as a replacement for validation mode

---
+++ src/RequestFiend.Models.Tests/PropertyTypes/ValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using RequestFiend.Models.PropertyTypes;
+using Xunit;
+
+namespace RequestFiend.Models.Tests.PropertyTypes;
+
+public class ValidatorTests {
+    [Theory]
+    [InlineData(null, false)]
+    [InlineData("", false)]
+    [InlineData(" ", true)]
+    [InlineData("Value", true)]
+    [InlineData("123", true)]
+    public void Required(string? value, bool expectedResult) {
+        Assert.Equal(expectedResult, Validator.Required(value));
+    }
+
+    [Theory]
+    [InlineData(null, false)]
+    [InlineData("", false)]
+    [InlineData(" ", false)]
+    [InlineData("Value", false)]
+    [InlineData("123", true)]
+    public void Numeric(string? value, bool expectedResult) {
+        Assert.Equal(expectedResult, Validator.Numeric(value));
+    }
+}
---
+++ src/RequestFiend.Models/PropertyTypes/Validator.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace RequestFiend.Models.PropertyTypes;
+
+public static class Validator {
+    public static bool Required(string? value) => !string.IsNullOrEmpty(value);
+    public static bool Numeric(string? value) => !string.IsNullOrEmpty(value) && !value.Any(c => !char.IsAsciiDigit(c));
+}
