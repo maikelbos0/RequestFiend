81a19655ef934f51753280d6370ab0d59515fc2d
Maikel Bos <maikel.bos0@gmail.com>
2026-02-11 21:01:20 +0100

    Add file model as a filePath string replacement in injection

---
+++ src/RequestFiend.Models.Tests/RequestTemplateCollectionFileModelTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace RequestFiend.Models.Tests;
+
+public class RequestTemplateCollectionFileModelTests {
+    [Fact]
+    public void Name() {
+        var subject = new RequestTemplateCollectionFileModel(@"C:\Path\To\File.json");
+
+        Assert.Equal("File", subject.Name);
+    }
+}
---
+++ src/RequestFiend.Models/RequestTemplateCollectionFileModel.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+
+namespace RequestFiend.Models;
+
+public record RequestTemplateCollectionFileModel(string FilePath) {
+    public string Name { get; } = Path.GetFileNameWithoutExtension(FilePath);
+};
