6335bbb25544b4b2ee1516518a71d7f0f2df7fb1
Maikel Bos <maikel.bos0@gmail.com>
2026-03-31 20:27:59 +0200

    Add variable and missing reference styles in overlay for a single entry as test

--- src/RequestFiend.UI/Views/ValidatableEntry.xaml.cs
+++ src/RequestFiend.UI/Views/ValidatableEntry.xaml.cs
@@ -1,18 +1,27 @@
 using Microsoft.Maui.Controls;
 using RequestFiend.Models.PropertyTypes;
+using RequestFiend.Models.Services;
+using System.Collections.Generic;
 
 namespace RequestFiend.UI.Views;
 
 public partial class ValidatableEntry : AbsoluteLayout {
     public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(ValidatableProperty<string>), typeof(ValidatableEntry), default(ValidatableProperty<string>));
+    public static readonly BindableProperty VariablesProperty = BindableProperty.Create(nameof(Variables), typeof(Dictionary<string, string>), typeof(ValidatableEntry), default(Dictionary<string, string>));
 
     public ValidatableProperty<string> Text {
         get => (ValidatableProperty<string>)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
     }
 
+    public Dictionary<string, string>? Variables {
+        get => GetValue(VariablesProperty) as Dictionary<string, string>;
+        set => SetValue(VariablesProperty, value);
+    }
+
     public ValidatableEntry() {
         InitializeComponent();
+        UpdateOverlay();
     }
 
     private void OnOverlayTapped(object sender, TappedEventArgs e) {
@@ -24,10 +33,36 @@ public partial class ValidatableEntry : AbsoluteLayout {
     }
 
     private void OnEntryFocused(object sender, FocusEventArgs e) {
-        Label.IsVisible = false;
+        Overlay.IsVisible = false;
     }
 
     private void OnEntryUnfocused(object sender, FocusEventArgs e) {
-        Label.IsVisible = true;
+        UpdateOverlay();
+    }
+
+    private void UpdateOverlay() {
+        if (Overlay.IsVisible = Variables != null) {
+            Overlay.FormattedText = null;
+            Overlay.FormattedText = new();
+
+            foreach (var span in VariableService.ProcessText(Text.Value, text => new Span() { Text = text }, CreateVariableReferenceSpan)) {
+                Overlay.FormattedText.Spans.Add(span);
+            }
+
+            Span CreateVariableReferenceSpan(string variableReference) {
+                var span = new Span() {
+                    Text = variableReference
+                };
+
+                if (Variables!.TryGetValue(variableReference.Trim('{', '}'), out var value)) {
+                    span.Style = (Style)Application.Current!.Resources["VariableReference"];
+                }
+                else {
+                    span.Style = (Style)Application.Current!.Resources["MissingReference"];
+                }
+
+                return span;
+            }
+        }
     }
 }
