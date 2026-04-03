f180a67fa5292e86006c46b0a7692788858da0bc
Maikel Bos <maikel.bos0@gmail.com>
2026-04-03 15:43:56 +0200

    Add variable formatting to all relevant entries

--- src/RequestFiend.UI/Views/NameValuePairCollectionView.xaml.cs
+++ src/RequestFiend.UI/Views/NameValuePairCollectionView.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.Controls;
+using RequestFiend.Core;
 using RequestFiend.Models;
 
 namespace RequestFiend.UI.Views;
@@ -8,6 +9,7 @@ public partial class NameValuePairCollectionView : ContentView {
     public static readonly BindableProperty NameLabelTextProperty = BindableProperty.Create(nameof(NameLabelText), typeof(string), typeof(NameValuePairCollectionView), default(string));
     public static readonly BindableProperty DeleteButtonTextProperty = BindableProperty.Create(nameof(DeleteButtonText), typeof(string), typeof(NameValuePairCollectionView), default(string));
     public static readonly BindableProperty AddButtonTextProperty = BindableProperty.Create(nameof(AddButtonText), typeof(string), typeof(NameValuePairCollectionView), default(string));
+    public static readonly BindableProperty CollectionProperty = BindableProperty.Create(nameof(Collection), typeof(RequestTemplateCollection), typeof(NameValuePairCollectionView), default(RequestTemplateCollection));
 
     public NameValuePairModelCollection ItemsSource {
         get => (NameValuePairModelCollection)GetValue(ItemsSourceProperty);
@@ -27,6 +29,11 @@ public partial class NameValuePairCollectionView : ContentView {
     public string AddButtonText {
         get => (string)GetValue(AddButtonTextProperty);
         set => SetValue(AddButtonTextProperty, value);
+    }
+
+    public RequestTemplateCollection? Collection {
+        get => GetValue(CollectionProperty) as RequestTemplateCollection;
+        set => SetValue(CollectionProperty, value);
     }
 
     public NameValuePairCollectionView() {
