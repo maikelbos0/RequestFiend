43eebb0ebf89e3c7a801722b9cbb2db001da4881
Maikel Bos <maikel.bos0@gmail.com>
2026-04-04 08:19:48 +0200

    Add full variable highlighting to content editor

--- src/RequestFiend.UI/Views/ValidatableContentEditor.xaml.cs
+++ src/RequestFiend.UI/Views/ValidatableContentEditor.xaml.cs
@@ -1,19 +1,45 @@
+using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Maui.Controls;
+using RequestFiend.Core;
+using RequestFiend.Models.Messages;
 using RequestFiend.Models.PropertyTypes;
+using RequestFiend.Models.Services;
 
 namespace RequestFiend.UI.Views;
 
 public partial class ValidatableContentEditor : AbsoluteLayout {
-    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(ValidatableProperty<string>), typeof(ValidatableContentEditor), default(ValidatableProperty<string>));
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(
+        nameof(Text),
+        typeof(ValidatableProperty<string>),
+        typeof(ValidatableContentEditor),
+        default(ValidatableProperty<string>),
+        propertyChanged: (bindable, _, _) => ((ValidatableContentEditor)bindable).UpdateOverlay()
+    );
+    public static readonly BindableProperty CollectionProperty = BindableProperty.Create(
+        nameof(Collection),
+        typeof(RequestTemplateCollection),
+        typeof(ValidatableContentEditor),
+        default(RequestTemplateCollection),
+        propertyChanged: (bindable, _, _) => ((ValidatableContentEditor)bindable).UpdateOverlay()
+    );
 
     public ValidatableProperty<string> Text {
         get => (ValidatableProperty<string>)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
     }
 
+    public RequestTemplateCollection? Collection {
+        get => GetValue(CollectionProperty) as RequestTemplateCollection;
+        set => SetValue(CollectionProperty, value);
+    }
+
     public ValidatableContentEditor() {
         InitializeComponent();
-        Overlay.IsVisible = true;
+        WeakReferenceMessenger.Default.Register<ValidatableContentEditor, RequestTemplateCollectionSettingsUpdatedMessage>(this, (_, message) => {
+            if (message.Collection == Collection) {
+                UpdateOverlay();
+            }
+        });
     }
 
     private void OnOverlayTapped(object sender, TappedEventArgs e) {
@@ -29,6 +55,44 @@ public partial class ValidatableContentEditor : AbsoluteLayout {
     }
 
     private void OnEditorUnfocused(object sender, FocusEventArgs e) {
-        Overlay.IsVisible = true;
+        UpdateOverlay();
+    }
+
+    private void UpdateOverlay() {
+        if (Collection != null && Text != null) {
+            var variables = Collection.GetVariables();
+            var hasVariables = false;
+
+            Overlay.IsVisible = true;
+            Overlay.FormattedText = null;
+            Overlay.FormattedText = new();
+
+            foreach (var span in VariableService.ProcessText(Text.Value, text => new Span() { Text = text, Style = (Style)Application.Current!.Resources["EditorOverlayText"] }, CreateVariableReferenceSpan)) {
+                Overlay.FormattedText.Spans.Add(span);
+            }
+
+            Span CreateVariableReferenceSpan(string variableReference) {
+                var span = new Span() {
+                    Text = variableReference
+                };
+
+                if (variables.TryGetValue(variableReference.Trim('{', '}'), out var value)) {
+                    hasVariables = true;
+                    span.Style = (Style)Application.Current!.Resources["EditorOverlayVariableReference"];
+                }
+                else {
+                    span.Style = (Style)Application.Current!.Resources["EditorOverlayMissingReference"];
+                }
+
+                return span;
+            }
+
+            if (hasVariables) {
+                ToolTipProperties.SetText(Overlay, Collection.ApplyVariables(Text.Value));
+            }
+            else {
+                ToolTipProperties.SetText(Overlay, default!);
+            }
+        }
     }
 }
