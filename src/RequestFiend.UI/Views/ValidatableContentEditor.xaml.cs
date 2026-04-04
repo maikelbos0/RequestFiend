fec610bc0133a44f39558150a0e6dd6f8a074fe6
Maikel Bos <maikel.bos0@gmail.com>
2026-04-04 07:56:20 +0200

    Add styles for editor overlay

--- src/RequestFiend.UI/Views/ValidatableContentEditor.xaml.cs
+++ src/RequestFiend.UI/Views/ValidatableContentEditor.xaml.cs
@@ -13,13 +13,22 @@ public partial class ValidatableContentEditor : AbsoluteLayout {
 
     public ValidatableContentEditor() {
         InitializeComponent();
+        Overlay.IsVisible = true;
     }
 
-    private void OnIconTapped(object sender, TappedEventArgs e) {
+    private void OnOverlayTapped(object sender, TappedEventArgs e) {
         Editor.Focus();
     }
 
-    private void OnIconPointerPressed(object sender, PointerEventArgs e) {
+    private void OnOverlayPointerPressed(object sender, PointerEventArgs e) {
         Editor.Focus();
+    }
+
+    private void OnEditorFocused(object sender, FocusEventArgs e) {
+        Overlay.IsVisible = false;
+    }
+
+    private void OnEditorUnfocused(object sender, FocusEventArgs e) {
+        Overlay.IsVisible = true;
     }
 }
