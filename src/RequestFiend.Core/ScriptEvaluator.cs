391ce4dd349d65873e8a0e3887874e4a7ff656b3
Maikel Bos <maikel.bos0@gmail.com>
2026-04-06 16:18:10 +0200

    Add System.Net.Http

--- src/RequestFiend.Core/ScriptEvaluator.cs
+++ src/RequestFiend.Core/ScriptEvaluator.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace RequestFiend.Core;
 
 public class ScriptEvaluator : IScriptEvaluator {
+    private static readonly ScriptOptions scriptOptions = ScriptOptions.Default.WithReferences([
+        typeof(HttpClient).Assembly
+    ]);
+
     public Task Evaluate(string script, RequestContext context, CancellationToken cancellationToken)
-        => CSharpScript.EvaluateAsync(script, globals: context, cancellationToken: cancellationToken);
+        => CSharpScript.EvaluateAsync(script, scriptOptions, context, cancellationToken: cancellationToken);
 }
