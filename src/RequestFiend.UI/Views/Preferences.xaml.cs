8ab40e880bf6e0b3fe87fd32997c041e4f8aedcc
Maikel Bos <maikel.bos0@gmail.com>
2026-04-08 09:59:58 +0200

    Rename preferences page for consistency

--- src/RequestFiend.UI/Views/Preferences.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using RequestFiend.Models;
-
-namespace RequestFiend.UI.Views;
-
-public partial class Preferences : ContentPage {
-    public Preferences(PreferencesModel model) : base(model) {
-        InitializeComponent();
-    }
-}
---
+++ src/RequestFiend.UI/Views/PreferencesPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using RequestFiend.Models;
+
+namespace RequestFiend.UI.Views;
+
+public partial class PreferencesPage : ContentPage {
+    public PreferencesPage(PreferencesModel model) : base(model) {
+        InitializeComponent();
+    }
+}
