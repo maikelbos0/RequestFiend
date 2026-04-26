2f1bbb29fb71af25a570a4c118848055b275613d
Maikel Bos <maikel.bos0@gmail.com>
2026-04-26 11:42:37 +0200

    Add new handler for managing certificate errors

---
+++ src/RequestFiend.Core.Tests/ServerCertificateValidationHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Security;
+using Xunit;
+
+namespace RequestFiend.Core.Tests;
+
+public class ServerCertificateValidationHandlerTests {
+    [Theory]
+    [InlineData(SslPolicyErrors.None, false, false, false, true)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable, false, false, false, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable, true, false, false, true)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNameMismatch, false, false, false, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNameMismatch, false, true, false, true)]
+    [InlineData(SslPolicyErrors.RemoteCertificateChainErrors, false, false, false, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateChainErrors, false, false, true, true)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable | SslPolicyErrors.RemoteCertificateNameMismatch | SslPolicyErrors.RemoteCertificateChainErrors, true, false, false, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable | SslPolicyErrors.RemoteCertificateNameMismatch | SslPolicyErrors.RemoteCertificateChainErrors, false, true, false, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable | SslPolicyErrors.RemoteCertificateNameMismatch | SslPolicyErrors.RemoteCertificateChainErrors, false, false, true, false)]
+    [InlineData(SslPolicyErrors.RemoteCertificateNotAvailable | SslPolicyErrors.RemoteCertificateNameMismatch | SslPolicyErrors.RemoteCertificateChainErrors, true, true, true, true)]
+    public void Handle(SslPolicyErrors sslPolicyErrors, bool ignoreRemoteCertificateNotAvailable, bool ignoreRemoteCertificateNameMismatch, bool ignoreRemoteCertificateChainErrors, bool expectedResult) {
+        var subject = new ServerCertificateValidationHandler() {
+            IgnoreRemoteCertificateNotAvailable = ignoreRemoteCertificateNotAvailable,
+            IgnoreRemoteCertificateNameMismatch = ignoreRemoteCertificateNameMismatch,
+            IgnoreRemoteCertificateChainErrors = ignoreRemoteCertificateChainErrors
+        };
+
+        Assert.Equal(expectedResult, subject.Handle(new(), null, null, sslPolicyErrors));
+    }
+}
---
+++ src/RequestFiend.Core/ServerCertificateValidationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RequestFiend.Core;
+
+public class ServerCertificateValidationHandler {
+    public bool IgnoreRemoteCertificateNotAvailable { get; set; }
+    public bool IgnoreRemoteCertificateNameMismatch { get; set; }
+    public bool IgnoreRemoteCertificateChainErrors { get; set; }
+
+    public bool Handle(HttpRequestMessage httpRequestMessage, X509Certificate2? x509Certificate2, X509Chain? x509Chain, SslPolicyErrors sslPolicyErrors) {
+        if (!IgnoreRemoteCertificateNotAvailable && sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateNotAvailable)) {
+            return false;
+        }
+
+        if (!IgnoreRemoteCertificateNameMismatch && sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateNameMismatch)) {
+            return false;
+        }
+
+        if (!IgnoreRemoteCertificateChainErrors && sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateChainErrors)) {
+            return false;
+        }
+
+        return true;
+    }
+}
