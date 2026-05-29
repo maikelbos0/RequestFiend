92e6ab2fde2890e7b3acced3119e74c0f7e5ff36
Maikel Bos <maikel.bos0@gmail.com>
2026-05-29 14:02:45 +0200

    Add content headers to request UI

--- src/RequestFiend.Models.Tests/HttpRequestModelTests.cs
+++ src/RequestFiend.Models.Tests/HttpRequestModelTests.cs
@@ -6,7 +6,7 @@ namespace RequestFiend.Models.Tests;
 
 public class HttpRequestModelTests {
     [Fact]
-    public void Create() {
+    public void Create_Without_Content() {
         var request = new HttpRequestMessage() {
             Method = HttpMethod.Post,
             RequestUri = new("https://localhost/"),
@@ -20,5 +20,23 @@ public class HttpRequestModelTests {
         Assert.Equal(request.Method.ToString(), subject.Method);
         Assert.Equal(request.RequestUri.ToString(), subject.Url);
         Assert.Equal(request.Headers.Count(), subject.Headers.Length);
+    }
+
+    [Fact]
+    public void Create_With_Content() {
+        var request = new HttpRequestMessage() {
+            Method = HttpMethod.Post,
+            RequestUri = new("https://localhost/"),
+            Headers = {
+                { "Accept", "application/json" }
+            },
+            Content = new StringContent("Content")
+        };
+
+        var subject = HttpRequestModel.Create(request);
+
+        Assert.Equal(request.Method.ToString(), subject.Method);
+        Assert.Equal(request.RequestUri.ToString(), subject.Url);
+        Assert.Equal(request.Headers.Count() + request.Content.Headers.Count(), subject.Headers.Length);
     }
 }
--- src/RequestFiend.Models/HttpRequestModel.cs
+++ src/RequestFiend.Models/HttpRequestModel.cs
@@ -4,6 +4,19 @@ using System.Net.Http;
 namespace RequestFiend.Models;
 
 public record HttpRequestModel(string Method, string? Url, HttpHeaderModel[] Headers) {
-    public static HttpRequestModel Create(HttpRequestMessage request)
-        => new(request.Method.ToString(), request.RequestUri?.ToString(), [.. request.Headers.Select(HttpHeaderModel.Create)]);
+    public static HttpRequestModel Create(HttpRequestMessage request) {
+        if (request.Content == null) {
+            return new(
+                request.Method.ToString(),
+                request.RequestUri?.ToString(),
+                [.. request.Headers.Select(HttpHeaderModel.Create)]
+            );
+        }
+
+        return new(
+            request.Method.ToString(),
+            request.RequestUri?.ToString(),
+            [.. request.Headers.Select(HttpHeaderModel.Create), .. request.Content.Headers.Select(HttpHeaderModel.Create)]
+        );
+    }
 }
