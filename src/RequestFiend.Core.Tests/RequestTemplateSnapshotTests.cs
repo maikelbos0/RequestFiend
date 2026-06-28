3f109a2e232cffa26283b74de0f9aa6ae1a1b4c0
Maikel Bos <maikel.bos0@gmail.com>
2026-06-28 13:49:39 +0200

    Add request message creation to snapshot

---
+++ src/RequestFiend.Core.Tests/RequestTemplateSnapshotTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+
+namespace RequestFiend.Core.Tests;
+
+public class RequestTemplateSnapshotTests {
+    [Theory]
+    [InlineData(ContentType.None, typeof(NoneContentManager))]
+    [InlineData(ContentType.Text, typeof(TextContentManager))]
+    [InlineData(ContentType.Json, typeof(JsonContentManager))]
+    [InlineData(ContentType.Xml, typeof(XmlContentManager))]
+    [InlineData(ContentType.File, typeof(FileContentManager))]
+    [InlineData(ContentType.FormData, typeof(FormDataContentManager))]
+    public void GetContentManager(ContentType contentType, Type expectedManagerType) {
+        var subject = new RequestTemplateSnapshot(
+            new([]),
+            "GET",
+            "https://localhost/",
+            [],
+            contentType,
+            false,
+            "StringContent",
+            "FileContent",
+            [],
+            [],
+            new([], "Code"),
+            new([], "Code"),
+            new([], "Code")
+        );
+
+        Assert.Equal(expectedManagerType, subject.GetContentManager().GetType());
+    }
+
+    [Fact]
+    public void CreateMessage_Creates_Message() {
+        var subject = new RequestTemplateSnapshot(
+            new([]),
+            "GET",
+            "https://localhost/",
+            [],
+            ContentType.None,
+            false,
+            "StringContent",
+            "FileContent",
+            [],
+            [],
+            new([], "Code"),
+            new([], "Code"),
+            new([], "Code")
+        );
+
+        var message = subject.CreateMessage();
+
+        Assert.Equal(subject.Method, message.Method.Method);
+        Assert.NotNull(message.RequestUri);
+        Assert.Equal(subject.Url, message.RequestUri.ToString());
+    }
+
+    [Fact]
+    public void CreateMessage_Applies_Variables() {
+        var subject = new RequestTemplateSnapshot(
+            new([
+                new("{{BaseUrl}}", "https://localhost:7001/")
+            ]),
+            "GET",
+            "{{baseurl}}values",
+            [],
+            ContentType.None,
+            false,
+            "StringContent",
+            "FileContent",
+            [],
+            [],
+            new([], "Code"),
+            new([], "Code"),
+            new([], "Code")
+        );
+
+        var message = subject.CreateMessage();
+
+        Assert.NotNull(message.RequestUri);
+        Assert.Equal("https://localhost:7001/values", message.RequestUri.ToString());
+    }
+
+    [Fact]
+    public void CreateMessage_Adds_Headers_Using_Variables() {
+        var subject = new RequestTemplateSnapshot(
+            new([
+                new("{{HeaderName}}", "Accept"),
+                new("{{HeaderValue}}", "application/json")
+            ]),
+            "GET",
+            "https://localhost:7001/",
+            [
+                new("{{HeaderName}}", "{{HeaderValue}}")
+            ],
+            ContentType.None,
+            false,
+            "StringContent",
+            "FileContent",
+            [],
+            [],
+            new([], "Code"),
+            new([], "Code"),
+            new([], "Code")
+        );
+
+        var message = subject.CreateMessage();
+
+        var header = Assert.Single(message.Headers);
+        Assert.Equal("Accept", header.Key);
+        Assert.Equal("application/json", Assert.Single(header.Value));
+    }
+
+    [Fact]
+    public void CreateMessage_Adds_Content_If_Available() {
+        var subject = new RequestTemplateSnapshot(
+            new([
+                new("{{BaseUrl}}", "https://localhost:7001/")
+            ]),
+            "GET",
+            "https://localhost:7001/",
+            [],
+            ContentType.Text,
+            false,
+            "Just a piece of text",
+            "FileContent",
+            [],
+            [],
+            new([], "Code"),
+            new([], "Code"),
+            new([], "Code")
+        );
+
+        var message = subject.CreateMessage();
+
+        Assert.IsType<StringContent>(message.Content);
+    }
+}
--- src/RequestFiend.Core/RequestTemplateSnapshot.cs
+++ src/RequestFiend.Core/RequestTemplateSnapshot.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
+using System.Net.Http;
 
 namespace RequestFiend.Core;
 
@@ -16,4 +18,26 @@ public record RequestTemplateSnapshot(
     ScriptSnapshot PreExchangeScript,
     ScriptSnapshot PostExchangeScript,
     ScriptSnapshot OnExceptionScript
-) { }
+) {
+    public IContentManager GetContentManager() => ContentType switch {
+        ContentType.None => new NoneContentManager(),
+        ContentType.Text => new TextContentManager(),
+        ContentType.Json => new JsonContentManager(),
+        ContentType.Xml => new XmlContentManager(),
+        ContentType.File => new FileContentManager(),
+        ContentType.FormData => new FormDataContentManager(),
+        _ => throw new NotImplementedException($"Received unknown content type '{ContentType}'.")
+    };
+
+    public HttpRequestMessage CreateMessage() {
+        var message = new HttpRequestMessage(HttpMethod.Parse(Method), new Uri(Variables.Apply(Url)));
+
+        foreach (var header in Headers) {
+            message.Headers.Add(Variables.Apply(header.Name), Variables.Apply(header.Value));
+        }
+
+        message.Content = GetContentManager().GetContent(this);
+
+        return message;
+    }
+}
