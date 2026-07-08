108ef94120d88e596d8af62ea56442c0588a6273
Maikel Bos <maikel.bos0@gmail.com>
2026-07-08 10:12:24 +0200

    Add GetActiveEnvironment

--- src/RequestFiend.Models.Tests/Services/EnvironmentServiceTests.cs
+++ src/RequestFiend.Models.Tests/Services/EnvironmentServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using RequestFiend.Core;
 using RequestFiend.Models.Services;
 using System.IO.Abstractions;
@@ -15,17 +16,115 @@ public class EnvironmentServiceTests {
         const string filePath = @"C:\Documents\Local.json";
 
         var fileSystem = Substitute.For<IFileSystem>();
-        var preferencesService = Substitute.For<IPreferencesService>();
         var environment = new Environment() {
             Variables = {
                 new() { Name = "DefaultHeader", Value = "Accept" }
             }
         };
 
-        var subject = new EnvironmentService(fileSystem, preferencesService);
+        var subject = new EnvironmentService(fileSystem, Substitute.For<IPreferencesService>(), Substitute.For<IPopupService>());
 
         await subject.Save(filePath, environment);
 
         await fileSystem.Received(1).File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(environment), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GetActiveEnvironment() {
+        const string filePath = @"C:\Documents\Local.json";
+
+        var fileSystem = Substitute.For<IFileSystem>();
+        var environment = new Environment() {
+            Variables = {
+                new() { Name = "DefaultHeader", Value = "Accept" }
+            }
+        };
+        fileSystem.File.Exists(filePath).Returns(true);
+        fileSystem.File.ReadAllTextAsync(filePath, Arg.Any<CancellationToken>()).Returns(JsonSerializer.Serialize(environment));
+        var preferencesService = Substitute.For<IPreferencesService>();
+        preferencesService.GetActiveEnvironment().Returns(new FileModel(filePath));
+        var popupService = Substitute.For<IPopupService>();
+
+        var subject = new EnvironmentService(fileSystem, preferencesService, popupService);
+
+        var result = await subject.GetActiveEnvironment();
+
+        Assert.Single(result.Variables);
+        await popupService.DidNotReceive().ShowErrorPopup(Arg.Any<string>());
+    }
+
+    [Fact]
+    public async Task GetActiveEnvironment_Caches() {
+        const string filePath = @"C:\Documents\Local.json";
+
+        var fileSystem = Substitute.For<IFileSystem>();
+        var environment = new Environment() {
+            Variables = {
+                new() { Name = "DefaultHeader", Value = "Accept" }
+            }
+        };
+        fileSystem.File.Exists(filePath).Returns(true);
+        fileSystem.File.ReadAllTextAsync(filePath, Arg.Any<CancellationToken>()).Returns(JsonSerializer.Serialize(environment));
+        var preferencesService = Substitute.For<IPreferencesService>();
+        preferencesService.GetActiveEnvironment().Returns(new FileModel(filePath));
+        var popupService = Substitute.For<IPopupService>();
+
+        var subject = new EnvironmentService(fileSystem, preferencesService, popupService);
+
+        _ = await subject.GetActiveEnvironment();
+        _ = await subject.GetActiveEnvironment();
+
+        preferencesService.Received(1).GetActiveEnvironment();
+        await fileSystem.File.Received(1).ReadAllTextAsync(filePath, Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GetActiveEnvironment_Without_Active_Environment() {
+        var preferencesService = Substitute.For<IPreferencesService>();
+        preferencesService.GetActiveEnvironment().ReturnsNull();
+        var popupService = Substitute.For<IPopupService>();
+
+        var subject = new EnvironmentService(Substitute.For<IFileSystem>(), preferencesService, popupService);
+
+        var result = await subject.GetActiveEnvironment();
+
+        Assert.Empty(result.Variables);
+        await popupService.DidNotReceive().ShowErrorPopup(Arg.Any<string>());
+    }
+
+    [Fact]
+    public async Task GetActiveEnvironment_Returns_Empty_For_Missing_File() {
+        const string filePath = @"C:\Documents\Local.json";
+
+        var fileSystem = Substitute.For<IFileSystem>();
+        fileSystem.File.Exists(filePath).Returns(false);
+        var preferencesService = Substitute.For<IPreferencesService>();
+        preferencesService.GetActiveEnvironment().Returns(new FileModel(filePath));
+        var popupService = Substitute.For<IPopupService>();
+
+        var subject = new EnvironmentService(fileSystem, preferencesService, popupService);
+
+        var result = await subject.GetActiveEnvironment();
+
+        Assert.Empty(result.Variables);
+        await popupService.Received(1).ShowErrorPopup(Arg.Any<string>());
+    }
+
+    [Fact]
+    public async Task GetActiveEnvironment_Returns_Empty_For_Deserialization_Error() {
+        const string filePath = @"C:\Documents\Local.json";
+
+        var fileSystem = Substitute.For<IFileSystem>();
+        fileSystem.File.Exists(filePath).Returns(true); fileSystem.File.ReadAllTextAsync(filePath, Arg.Any<CancellationToken>()).Returns("Invalid JSON");
+        var preferencesService = Substitute.For<IPreferencesService>();
+        preferencesService.GetActiveEnvironment().Returns(new FileModel(filePath));
+        var popupService = Substitute.For<IPopupService>();
+
+        var subject = new EnvironmentService(fileSystem, preferencesService, popupService);
+
+        var result = await subject.GetActiveEnvironment();
+
+        Assert.Empty(result.Variables);
+        await popupService.Received(1).ShowErrorPopup(Arg.Any<string>());
     }
 }
--- src/RequestFiend.Models/Services/EnvironmentService.cs
+++ src/RequestFiend.Models/Services/EnvironmentService.cs
@@ -8,12 +8,38 @@ namespace RequestFiend.Models.Services;
 public class EnvironmentService : IEnvironmentService {
     private readonly IFileSystem fileSystem;
     private readonly IPreferencesService preferencesService;
+    private readonly IPopupService popupService;
+    private Environment? activeEnvironment;
 
-    public EnvironmentService(IFileSystem fileSystem, IPreferencesService preferencesService) {
+    public EnvironmentService(IFileSystem fileSystem, IPreferencesService preferencesService, IPopupService popupService) {
         this.fileSystem = fileSystem;
         this.preferencesService = preferencesService;
+        this.popupService = popupService;
     }
 
     public Task Save(string filePath, Environment environment)
         => fileSystem.File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(environment));
+
+
+    public async Task<Environment> GetActiveEnvironment() {
+        if (activeEnvironment == null) {
+            var filePath = preferencesService.GetActiveEnvironment()?.FilePath;
+
+            if (filePath != null) {
+                if (fileSystem.File.Exists(filePath)) {
+                    try {
+                        activeEnvironment = JsonSerializer.Deserialize<Environment>(await fileSystem.File.ReadAllTextAsync(filePath));
+                    }
+                    catch (System.Exception exception) {
+                        await popupService.ShowErrorPopup($"Failed to load active environment: {exception.Message}");
+                    }
+                }
+                else {
+                    await popupService.ShowErrorPopup("Active environment file does not exist.");
+                }
+            }
+        }
+
+        return activeEnvironment ??= new();
+    }
 }
