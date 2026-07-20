fc3eba6b18a4c8db23489700dbd28b694e0f8980
Maikel Bos <maikel.bos0@gmail.com>
2026-07-20 19:11:08 +0200

    Add highlighting for selected item while dragging

---
+++ src/RequestFiend.Models.Tests/Converters/SelectedItemStyleConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Maui.Controls;
+using RequestFiend.Models.Converters;
+using Xunit;
+
+namespace RequestFiend.Models.Tests.Converters;
+
+public class SelectedItemStyleConverterTests {
+    [Fact]
+    public void Convert_Returns_Style_If_Equal() {
+        var subject = new SelectedItemStyleConverter();
+        var style = new Style(typeof(Label));
+
+        var result = subject.Convert([42, 42, style], default!, default!, default!);
+
+        Assert.Equal(style, result);
+    }
+
+    [Fact]
+    public void Convert_Returns_Null_If_Unequal() {
+        var subject = new SelectedItemStyleConverter();
+        var style = new Style(typeof(Label));
+
+        var result = subject.Convert([42, 41, style], default!, default!, default!);
+
+        Assert.Null(result);
+    }
+
+
+    [Fact]
+    public void Convert_Returns_Null_With_Invalid_Style() {
+        var subject = new SelectedItemStyleConverter();
+        var style = "LabelStyle";
+
+        var result = subject.Convert([42, 42, style], default!, default!, default!);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Convert_Returns_Null_If_Too_Few_Arguments() {
+        var subject = new SelectedItemStyleConverter();
+
+        var result = subject.Convert([42, 42], default!, default!, default!);
+
+        Assert.Null(result);
+    }
+}
---
+++ src/RequestFiend.Models/Converters/SelectedItemStyleConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace RequestFiend.Models.Converters;
+
+public class SelectedItemStyleConverter : IMultiValueConverter {
+    public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+        if (values.Length < 3 || values[2] is not Style style || !Equals(values[0], values[1])) {
+            return null;
+        }
+
+        return style;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
