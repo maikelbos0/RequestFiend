7af7bf429f21bb0c7796d3c222c1e25060e93cd9
Maikel Bos <maikel.bos0@gmail.com>
2026-07-22 20:25:06 +0200

    Set up basic console app

---
+++ src/RequestFiend.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CommandLine;
+using System.IO;
+using System.Threading.Tasks;
+
+var collectionArgument = new Argument<FileInfo>("collection") {
+    Description = "Collection from which to execute requests"
+};
+var rootCommand = new RootCommand("RequestFiend - An open source platform for managing and executing API requests.");
+rootCommand.Arguments.Add(collectionArgument);
+rootCommand.SetAction((parseResult, cancellationToken) => {
+    var collectionFile = parseResult.GetValue(collectionArgument) ?? throw new InvalidOperationException();
+
+    Console.WriteLine(collectionFile.FullName);
+
+    return Task.CompletedTask;
+});
+
+await rootCommand.Parse(args).InvokeAsync();
