65a32bb11ec3956336dbd619ac0ff9d473d2e7ce
Maikel Bos <maikel.bos0@gmail.com>
2026-07-25 08:38:59 +0200

    Add parser for JSON data

--- src/RequestFiend.Console.Tests/ParsersTests.cs
+++ src/RequestFiend.Console.Tests/ParsersTests.cs
@@ -5,6 +5,44 @@ using Xunit;
 namespace RequestFiend.Console.Tests;
 
 public class ParsersTests {
+    private sealed record Data(string Value);
+
+    [Fact]
+    public void CreateJsonFileParser_When_Valid() {
+        var option = new Option<Data>("--data") {
+            CustomParser = Parsers.CreateJsonFileParser<Data>("option '--data'"),
+            Arity = ArgumentArity.ZeroOrMore,
+            AllowMultipleArgumentsPerToken = true
+        };
+
+        var command = new RootCommand() { option };
+
+        var result = command.Parse(["--data", "./Data.json"]);
+
+        Assert.Empty(result.Errors);
+        Assert.Equal(new("Foo"), result.GetValue(option));
+    }
+
+    [Theory]
+    [InlineData(new string[] { }, "Missing required argument for option '--data'.")]
+    [InlineData(new string[] { "./Data.json", "./Empty.json" }, "Received too many arguments for option '--data'.")]
+    [InlineData(new string[] { "./Missing.json" }, "Argument for option '--data' must be an existing file.")]
+    [InlineData(new string[] { "./Empty.json" }, "Argument for option '--data' must be a valid JSON file.")]
+    [InlineData(new string[] { "./Invalid.json" }, "Argument for option '--data' must be a valid JSON file: 'I' is an invalid start of a value. Path: $ | LineNumber: 0 | BytePositionInLine: 0.")]
+    public void CreateJsonFileParser_When_Invalid(string[] arguments, string expectedError) {
+        var option = new Option<Data>("--data") {
+            CustomParser = Parsers.CreateJsonFileParser<Data>("option '--data'"),
+            Arity = ArgumentArity.ZeroOrMore,
+            AllowMultipleArgumentsPerToken = true
+        };
+
+        var command = new RootCommand() { option };
+
+        var result = command.Parse(["--data", .. arguments]);
+
+        Assert.Equivalent(expectedError, Assert.Single(result.Errors).Message);
+    }
+
     [Theory]
     [InlineData("1", 1)]
     [InlineData("123", 123)]
--- src/RequestFiend.Console/Parsers.cs
+++ src/RequestFiend.Console/Parsers.cs
@@ -1,9 +1,44 @@
 ﻿using System;
 using System.CommandLine.Parsing;
+using System.IO;
+using System.Text.Json;
 
 namespace RequestFiend.Console;
 
 public static class Parsers {
+    public static Func<ArgumentResult, TValue?> CreateJsonFileParser<TValue>(string name) where TValue : class
+        => (ArgumentResult result) => {
+            if (result.Tokens.Count == 0) {
+                result.AddError($"Missing required argument for {name}.");
+                return null;
+            }
+
+            if (result.Tokens.Count > 1) {
+                result.AddError($"Received too many arguments for {name}.");
+                return null;
+            }
+
+            if (!File.Exists(result.Tokens[0].Value)) {
+                result.AddError($"Argument for {name} must be an existing file.");
+                return null;
+            }
+
+            try {
+                var value = JsonSerializer.Deserialize<TValue>(File.ReadAllText(result.Tokens[0].Value));
+
+                if (value == null) {
+                    result.AddError($"Argument for {name} must be a valid JSON file.");
+                    return null;
+                }
+
+                return value;
+            }
+            catch (Exception exception) {
+                result.AddError($"Argument for {name} must be a valid JSON file: {exception.Message}");
+                return null;
+            }
+        };
+
     public static Func<ArgumentResult, TimeSpan?> CreateSecondsParser(string name)
         => (ArgumentResult result) => {
             if (result.Tokens.Count == 0) {
