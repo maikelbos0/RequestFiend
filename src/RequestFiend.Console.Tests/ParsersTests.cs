9ecb9b444c6eb90a516b0539bee4de79b6faa591
Maikel Bos <maikel.bos0@gmail.com>
2026-07-25 08:27:28 +0200

    Simplify tests

--- src/RequestFiend.Console.Tests/ParsersTests.cs
+++ src/RequestFiend.Console.Tests/ParsersTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.CommandLine;
-using System.Linq;
 using Xunit;
 
 namespace RequestFiend.Console.Tests;
@@ -12,7 +11,8 @@ public class ParsersTests {
     public void CreateSecondsParser_When_Valid(string argument, int expectedValueInSeconds) {
         var option = new Option<TimeSpan?>("--seconds") {
             CustomParser = Parsers.CreateSecondsParser("option '--seconds'"),
-            Arity = ArgumentArity.ZeroOrMore
+            Arity = ArgumentArity.ZeroOrMore,
+            AllowMultipleArgumentsPerToken = true
         };
 
         var command = new RootCommand() { option };
@@ -24,14 +24,15 @@ public class ParsersTests {
     }
 
     [Theory]
-    [InlineData("option '--seconds'", new string[] { }, new string[] { "Missing required argument for option '--seconds'" })]
-    [InlineData("option '--seconds'", new string[] { "1", "1" }, new string[] { "Received too many arguments for option '--seconds'" })]
-    [InlineData("option '--seconds'", new string[] { "" }, new string[] { "Argument for option '--seconds' must be a positive number of seconds" })]
-    [InlineData("option '--seconds'", new string[] { "1a" }, new string[] { "Argument for option '--seconds' must be a positive number of seconds" })]
-    [InlineData("option '--seconds'", new string[] { "0" }, new string[] { "Argument for option '--seconds' must be a positive number of seconds" })]
-    public void CreateSecondsParser_When_Invalid(string name, string[] arguments, string[] expectedErrors) {
+    [InlineData(new string[] { }, "Missing required argument for option '--seconds'.")]
+    [InlineData(new string[] { "1", "1" }, "Received too many arguments for option '--seconds'.")]
+    [InlineData(new string[] { "" }, "Argument for option '--seconds' must be a positive number of seconds.")]
+    [InlineData(new string[] { "1a" }, "Argument for option '--seconds' must be a positive number of seconds.")]
+    [InlineData(new string[] { "0" }, "Argument for option '--seconds' must be a positive number of seconds.")]
+    [InlineData(new string[] { "-1" }, "Argument for option '--seconds' must be a positive number of seconds.")]
+    public void CreateSecondsParser_When_Invalid(string[] arguments, string expectedError) {
         var option = new Option<TimeSpan?>("--seconds") {
-            CustomParser = Parsers.CreateSecondsParser(name),
+            CustomParser = Parsers.CreateSecondsParser("option '--seconds'"),
             Arity = ArgumentArity.ZeroOrMore,
             AllowMultipleArgumentsPerToken = true
         };
@@ -40,6 +41,6 @@ public class ParsersTests {
 
         var result = command.Parse(["--seconds", .. arguments]);
 
-        Assert.Equivalent(expectedErrors, result.Errors.Select(error => error.Message));
+        Assert.Equivalent(expectedError, Assert.Single(result.Errors).Message);
     }
 }
--- src/RequestFiend.Console/Parsers.cs
+++ src/RequestFiend.Console/Parsers.cs
@@ -7,17 +7,17 @@ public static class Parsers {
     public static Func<ArgumentResult, TimeSpan?> CreateSecondsParser(string name)
         => (ArgumentResult result) => {
             if (result.Tokens.Count == 0) {
-                result.AddError($"Missing required argument for {name}");
+                result.AddError($"Missing required argument for {name}.");
                 return null;
             }
 
             if (result.Tokens.Count > 1) {
-                result.AddError($"Received too many arguments for {name}");
+                result.AddError($"Received too many arguments for {name}.");
                 return null;
             }
-            
+
             if (!int.TryParse(result.Tokens[0].Value, out var seconds) || seconds < 1) {
-                result.AddError($"Argument for {name} must be a positive number of seconds");
+                result.AddError($"Argument for {name} must be a positive number of seconds.");
                 return null;
             }
 
