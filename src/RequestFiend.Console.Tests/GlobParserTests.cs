d500c1e268209641993cbb52c9d092d3b7a0a274
Maikel Bos <maikel.bos0@gmail.com>
2026-08-01 09:04:41 +0200

    Start building a simple glob parser

---
+++ src/RequestFiend.Console.Tests/GlobParserTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace RequestFiend.Console.Tests;
+
+public class GlobParserTests {
+    [Theory]
+    [InlineData("Test", true, "Test")]
+    [InlineData("*Test", true, ".*Test")]
+    [InlineData("Test?", true, "Test.")]
+    public void TryParse(string input, bool expectedResult, string? expectedPattern) {
+        var subject = new GlobParser();
+
+        var result = subject.TryParse(input, out var pattern);
+
+        Assert.Equal(expectedResult, result);
+        Assert.Equal(expectedPattern, pattern);
+    }
+}
---
+++ src/RequestFiend.Console/GlobParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace RequestFiend.Console;
+
+public class GlobParser {
+    private const char singleCharacterMatch = '?';
+    private const char zeroOrMoreCharactersMatch = '*';
+
+    // . $ ^ { [ ( | ) * + ? \
+    public bool TryParse(string input, [NotNullWhen(true)] out string? pattern) {
+        var patternBuilder = new StringBuilder();
+        
+        for (var i = 0; i < input.Length; i++) {
+            if (input[i] == singleCharacterMatch) {
+                patternBuilder.Append('.');
+            }
+            else if (input[i] == zeroOrMoreCharactersMatch) {
+                patternBuilder.Append(".*");
+            }
+            else {
+                patternBuilder.Append(input[i]);
+            }
+        }
+
+        pattern = patternBuilder.ToString();
+        return true;
+    }
+}
