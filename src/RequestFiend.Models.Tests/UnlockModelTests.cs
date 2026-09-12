e1918a7700f9cc5dade10d2dbade7dc109b29e89
Maikel Bos <maikel.bos0@gmail.com>
2026-09-12 09:48:01 +0200

    Add model for unlocking

---
+++ src/RequestFiend.Models.Tests/UnlockModelTests.cs
@@ -0,0 +1,55 @@
+﻿using NSubstitute;
+using RequestFiend.Core;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RequestFiend.Models.Tests;
+
+public class UnlockModelTests : TestsBase {
+    [Fact]
+    public void Constructor() {
+        var subject = new UnlockModel(Substitute.For<Func<CancellationToken, Task>>(), Substitute.For<ISecretEncryptor>(), Substitute.For<ISecretOwner>());
+        
+        Assert.Empty(subject.Password.Value);
+
+        Assert.Equal([subject.Password], subject.Validatables);
+    }
+
+    [Fact]
+    public async Task TryUnlock_With_Valid_Password() {
+        var closeMethod = Substitute.For<Func<CancellationToken, Task>>();
+        var secretEncryptor = Substitute.For<ISecretEncryptor>();
+        var owner = Substitute.For<ISecretOwner>();
+        secretEncryptor.TryUnlock(Arg.Any<ISecretOwner>(), Arg.Any<string>()).Returns(true);
+
+        var subject = new UnlockModel(closeMethod, secretEncryptor, owner) {
+            Password = { Value = "password" }
+        };
+
+        await subject.TryUnlock(CancellationToken.None);
+
+        secretEncryptor.Received().TryUnlock(owner, "password");
+
+        await closeMethod.Received().Invoke(CancellationToken.None);
+    }
+
+    [Fact]
+    public async Task TryUnlock_With_Invalid_Password() {
+        var closeMethod = Substitute.For<Func<CancellationToken, Task>>();
+        var secretEncryptor = Substitute.For<ISecretEncryptor>();
+        var owner = Substitute.For<ISecretOwner>();
+        secretEncryptor.TryUnlock(Arg.Any<ISecretOwner>(), Arg.Any<string>()).Returns(false);
+
+        var subject = new UnlockModel(closeMethod, secretEncryptor, owner) {
+            Password = { Value = "password" }
+        };
+
+        await subject.TryUnlock(CancellationToken.None);
+
+        secretEncryptor.Received().TryUnlock(owner, "password");
+
+        await closeMethod.DidNotReceive().Invoke(Arg.Any<CancellationToken>());
+    }
+}
---
+++ src/RequestFiend.Models/UnlockModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RequestFiend.Core;
+using RequestFiend.Models.PropertyTypes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequestFiend.Models;
+
+public partial class UnlockModel : BoundModelBase {
+    private readonly Func<CancellationToken, Task> closeMethod;
+    private readonly ISecretEncryptor secretEncryptor;
+    private readonly ISecretOwner owner;
+    
+    public ValidatableProperty<string> Password { get; }
+    [ObservableProperty] public partial bool IsValid { get; set; }
+
+    public UnlockModel(Func<CancellationToken, Task> closeMethod, ISecretEncryptor secretEncryptor, ISecretOwner owner) {
+        this.closeMethod = closeMethod;
+        this.secretEncryptor = secretEncryptor;
+        this.owner = owner;
+
+        Password = new(() => "", _ => { }, Validator.Required);
+
+        ConfigureState([Password]);
+    }
+
+    [RelayCommand]
+    public async Task TryUnlock(CancellationToken cancellationToken) {
+        if (HasError) {
+            return;
+        }
+
+        IsValid = secretEncryptor.TryUnlock(owner, Password.Value);
+
+        if (IsValid) {
+            await closeMethod(cancellationToken);
+        }
+    }
+}
